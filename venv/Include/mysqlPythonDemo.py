d3099ca09b866538a4f7088b51036358de25e2a2
Hirror <hhh9097@163.com>
2018-03-29 17:26:59 +0800

    Update by Hirror python study demo mysql

---
+++ venv/Include/mysqlPythonDemo.py
@@ -0,0 +1,19 @@
+
+#!/usr/bin/env python
+
+# encoding: utf-8
+
+'''
+
+@author: Hirror
+
+@contact: hanhh@hefupb.com
+
+@file: mysqlPythonDemo.py
+
+@time: 2018/3/29 17:25
+
+@desc:
+
+'''
+import MySQLdb
